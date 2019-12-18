9fcd2d2db3a480d51a61a31b2b2413aaca44d2e4
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-18 18:42:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,14 @@ class Student
   end
   
   def self.find_by_name(name)
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+    SQL
+    row = DB[:conn].execute(sql, name)
+    new_from_db(row[0])
+  end
 
 
 end
