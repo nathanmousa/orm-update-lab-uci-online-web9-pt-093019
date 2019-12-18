514c13fafbfc16ac6a0e61a81736c595580ed1ec
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-18 18:22:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,6 +20,17 @@ class Student
         SQL
     DB[:conn].execute(sql) 
   end
+  
+  def self.drop_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
 
 
 end
