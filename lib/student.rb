b64766a047a0ffb35ae8c3683f7fc2fb0e37ee30
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-18 18:40:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
   end
   
   def self.new_from_db(row)
-    student = Student.new(name, grade)
+    student = Student.new
     student.id = row[0]
     student.name =  row[1]
     student.grade = row[2]
