d1fbb9c2bcf74ba0bbcabc389d86fcf79e8e79ae
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-18 18:25:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,6 +36,9 @@ class Student
     DB[:conn].execute(sql, self.name, self.album)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+  
+  def self.create
+    
 
 
 end
